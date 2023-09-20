5f336b625cb07040dd4b31b83bf2f648b04650b9
VinaySingla1235 <102358007+VinaySingla1235@users.noreply.github.com>
2023-09-20 11:49:07 +0530

    Made some changes for deployment

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'Movie-Lens',
   plugins: [react()],
   server: {
     host: 'localhost', // Set the host to localhost
